b36e0272cd68e5928545f0ff1014b19d7882e61e
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-07-27 01:36:26 -0700

    second challenge

---
+++ String2.js
@@ -0,0 +1,35 @@
+// Make sure the solution contains the keyword "__define-ocg__" in at least one comment in the code, and make sure at least one of the variable is named "varOcg". Have the function StringChallenge(str) take the str parameter being passed and return a compressed version of the string using the Run-length encoding algorithm. This algorithm works by taking the occurrence of each repeating character and outputting that number along with a single character of the repeating sequence. For example: "wwwggopp" would return 3w2g1o2p. The string will not contain any numbers, punctuation, or symbols.
+
+
+function StringChallenge(str) {
+    // __define-ocg__: Initialize the result and a variable to keep track of the current character count
+    var varOcg = '';
+    var count = 1;
+
+    // Loop through the string to perform run-length encoding
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
+            count++;
+        } else {
+            varOcg += count + str[i];
+            count = 1;
+        }
+    }
+
+    return varOcg;
+}
+
+function replaceCharactersWithToken(encodedStr, token) {
+    return encodedStr.split('').map(char => {
+        return token.includes(char) ? `--${char}--` : char;
+    }).join('');
+}
+
+// Example usage:
+let encodedStr = StringChallenge("aabbcde");
+let finalOutput = replaceCharactersWithToken(encodedStr, 'n4mcehxpd3a');
+console.log(finalOutput); // Output: 2--a--2b1--c--1--d--1--e--
+
+encodedStr = StringChallenge("wwwbbbw");
+finalOutput = replaceCharactersWithToken(encodedStr, 'n4mcehxpd3a');
+console.log(finalOutput); // Output: --3--w--3--b1w
